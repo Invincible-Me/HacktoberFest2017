98bf0e3f80ecc3acab218cb8f45c86ba069c7ecb
mdeyell <mdeyell@ucsc.du>
2017-10-08 12:06:47 -0700

    got html page to display

--- resume.js
+++ resume.js
@@ -1,10 +1,47 @@
 /*Hello World*/
 console.log("Hello, World!");
 
-var http = require('http');
 
-http.createServer(function (req, res) {
-	    res.writeHead(200, {'Content-Type': 'text/plain'});
-	        res.end('Hello World!');
-}).listen(3000);
+
+
+
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/resume.html'));
+});
+
+app.listen(3000);
+
+
+
+console.log("Running at Port 3000");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
