fdc06fa1a745bc2c0428c5bba2485e31dfe7c01e
mdeyell <mdeyell@ucsc.du>
2017-10-08 16:52:16 -0700

    fixed html issue with not all checked values being in place. also updated javascript response string

--- resume.js
+++ resume.js
@@ -18,49 +18,41 @@ app.get('/',function(req,res){
 app.post('/submit', function (req, res) {
 
 	var responseString = "";
-
-	//if a checkbox is not checked it will be undefined
-	response = {
-		form_data:req.body
-	};
-	console.log(response);
-
-    //if haveResumeYes is defined
-	if(req.body.haveResumeYes){
-		console.log("yes value is selected")
-	}
-	
-	//if haveResumeNo is defined
-	if(req.body.haveResumeNo){
-		console.log("no value is selected")
-	}
 	//console.log("contact info is: "+req.body.contact);
 	//console.log("type of contact info is: "+typeof(req.body.contact));
 	var hasContact = req.body.contact.toUpperCase() === "yes".toUpperCase();
 	if(hasContact){
-         console.log("Resume has contact info");
+           //console.log("Resume has contact info");
 	}else{
-        console.log("Resume has no contact info");
+           // console.log("Resume has no contact info");
+	   responseString += "<br>If you don't put your contact info, how will your employer tell you that you got the job?";
 	}
 
 	var hasWork=req.body.work.toUpperCase() === "yes".toUpperCase();
         //console.log("work experience info is: "+req.body.work);
-	        if(hasWork){
-	         console.log("Resume has work info");
-	        }else{
-	         console.log("Resume has no work info");
-	         }   
+	if(hasWork){
+	   //console.log("Resume has work info");
+	}else{
+	   //console.log("Resume has no work info");
+	   responseString += "<br>It is very important to include your previous work history";
+	}   
 
 	var hasObjective = (req.body.objective.toUpperCase() === "YES");
 	if(!hasObjective){
-		responseString += "<br>An Objective statement highlights the purpose of a resume"
+		responseString += "<br>An Objective statement highlights the purpose of a resume";
 	}
 
 	var haEducation = (req.body.education.toUpperCase() === "YES");
 	if(!hasObjective){
-		responseString += "<br>Your highest degree will help Employeers see your educational achievements"
+		responseString += "<br>Your highest degree will help Employeers see your educational achievements";
 	}	
-
+	
+	//final response string formatting
+	if(responseString.length===0){
+                responseString = "<br>Your resume is in good shape! You got this"
+	}else{
+                responseString = "You should consider the following feedback:"+responseString;
+        }
 	res.send(responseString);
 })
 
